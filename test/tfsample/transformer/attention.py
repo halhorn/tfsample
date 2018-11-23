04e704fab64f704cfe5c870fc6f9517f1c62779d
harumitsu.nobuta <harumitsu.nobuta@mixi.co.jp>
2018-11-23 11:54:16 +0900

    test train

--- test/tfsample/transformer/attention.py
+++ test/tfsample/transformer/attention.py
@@ -75,3 +75,37 @@ class TestTransformer(unittest.TestCase):
         layer = MultiheadAttention(head_num=head_num)
         result = layer._split_head(tf.ones(shape=[batch_size, max_len, dim]))
         self.assertEqual(result.shape, [batch_size, head_num, max_len, int(dim / head_num)])
+
+    @unittest.skip('debugging')
+    def test_train(self):
+        head_num = 8
+        dim = 256
+        batch_size = 128
+
+        layer = MultiheadAttention(head_num=head_num, k_dim=dim, v_dim=dim, o_dim=3)
+
+        def loss(query, memory, labels):
+            result = layer([tf.expand_dims(query, 1), memory, memory])
+            result = tf.squeeze(result, [1])
+            return tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=result)
+
+        def grad(query, memory, labels):
+            with tf.GradientTape() as tape:
+                loss_val = loss(query, memory, labels)
+            return loss_val, tape.gradient(loss_val, layer.weights)
+
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.0001)
+        global_step = tf.train.get_or_create_global_step()
+
+        last_loss = 100
+        for _ in range(10000):
+            query = tf.random_uniform([batch_size, 3], -1.0, 1.0)
+            memory = tf.random_uniform([batch_size, 4, 3], -1.0, 1.0)
+            s = tf.reduce_sum(query, -1) + tf.reduce_sum(memory, [1, 2])
+            labels = tf.cast(s > 0, tf.int32)
+            loss_val, grad_val = grad(query, memory, labels)
+            optimizer.apply_gradients(zip(grad_val, layer.weights), global_step)
+            if loss_val > last_loss:
+                batch_size *= 2
+            if int(global_step) % 100 == 0:
+                print(global_step.numpy(), loss_val.numpy())
